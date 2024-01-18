355337e4435811816fdedacea58f68810b55bb40
Hamidullo <tuychibaevhamidullo1999@gmail.com>
2024-01-18 09:32:27 +0500

    First Commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div>
+      <button @click="openWindow">Open me</button>
+      <button @click="closeWindow">Close me</button>
+      <a href="#" onclick="javascript:window.close();opener.window.focus();" >Close Window</a>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data()
+  {
+    return{
+      url:'https://hemis.nammti.uz/oauth/authorize?response_type=code&client_id=3&redirect_uri=https%3A%2F%2Fosiyohometex.uz%2F',
+      result:'',
+    }
+  },
+  methods:{
+    openWindow(){
+
+      window.open(this.url,)
+    },
+    closeWindow(){
+      window.open('','_parent','');
+      window.close();
+    }
+  }
+}
+</script>
